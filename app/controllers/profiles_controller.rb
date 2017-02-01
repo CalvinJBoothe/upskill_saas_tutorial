393fc95915b0d42648bd39f6554ffe21ccc5cc96
Calvin Boothe <cjb339@nau.edu>
2017-02-01 01:02:54 +0000

    Added user profiles form

--- app/controllers/profiles_controller.rb
+++ app/controllers/profiles_controller.rb
@@ -3,5 +3,6 @@ class ProfilesController < ApplicationController
   # When a user makes a get request to /users/:user_id/profile/new
   def new
     # Render blank profile details form
+    @profile = Profile.new
   end
 end
